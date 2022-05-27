e24cdd53a5b1a16348eb5bebc515bee26d868289
Lucas-OF28 <luuu_of@hotmail.com>
2022-05-27 15:41:00 -0300

    formulario de postagens

--- routes/admin.js
+++ routes/admin.js
@@ -98,5 +98,18 @@ router.get("/categorias/edit/:id", (req, res) => {
       res.redirect("admin/categorias")
     })
   })
+  
+  router.get("/postagens", (req, res) => {
+    res.render("admin/postagens")
+  })
+
+  router.get("/postagens/add", (req, res) =>{
+    Categoria.find().lean().then((categorias) => {
+      res.render("admin/addpostagem", {categorias: categorias})
+    }).catch((err) =>{
+      req.flash("error_msg", "Houve um erro ao carregar o formulario")
+      res.redirect("/admin")
+    })
+  })
 
 module.exports = router
