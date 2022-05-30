5be02d2b66415b9f8509ad644a5932725fa54bdb
Lucas-OF28 <luuu_of@hotmail.com>
2022-05-30 18:15:26 -0300

    pagina de postagens

--- app.js
+++ app.js
@@ -49,6 +49,27 @@ const Postagem = mongoose.model("postagens")
             res.redirect("/404")
         })
     })
+
+    app.get("/postagem/:slug", (req,res) => {
+        const slug = req.params.slug
+        Postagem.findOne({slug}).then((postagem) => {
+            if(postagem){
+                const post = {
+                    titulo: postagem.titulo,
+                    data: postagem.data,
+                    conteudo: postagem.conteudo
+                }
+                res.render("postagem/index", post)
+            }else{
+                req.flash("error_msg", "Esta postagem não existe")
+                res.redirect("/")
+            }
+        }).catch((err) => {
+            req.flash("error_msg", "Houve um erro interno")
+            res.redirect("/")
+        })
+    })
+
     app.get('/404',(req,res) => {
         res.send("Erro 404!")
     })
