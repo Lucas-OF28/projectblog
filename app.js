bc9f7a21bce3df5f3a41e3c30c042080b80241bc
Lucas-OF28 <luuu_of@hotmail.com>
2022-05-30 17:55:03 -0300

    criando homepage

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const path = require("path")
 const mongoose = require("mongoose")
 const session = require('express-session')
 const flash = require('connect-flash')
+require("./models/Postagem")
+const Postagem = mongoose.model("postagens")
 //Config
     //Sessao
         app.use(session({
@@ -40,7 +42,15 @@ const flash = require('connect-flash')
         app.use(express.static(path.join(__dirname,"public")))
 //Routes
     app.get('/', (req,res) => {
-        res.send("Rota principal")
+        Postagem.find().populate("categoria").sort({data: "desc"}).lean().then((postagens) => {
+            res.render("index", {postagens: postagens})
+        }).catch((err) => {
+            req.flash("error_msg", "Houve um erro interno")
+            res.redirect("/404")
+        })
+    })
+    app.get('/404',(req,res) => {
+        res.send("Erro 404!")
     })
     app.get('/posts', (req,res) => {
         res.send("Lista de posts")
